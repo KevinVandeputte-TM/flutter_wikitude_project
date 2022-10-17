2e3b2c40f886799d9c3f864c2878d08adc755b85
Kevin Vandeputte <r0792871@student.thomasmore.be>
2022-10-17 19:37:09 +0200

    basic home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,26 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:got_app/pages/highscores.dart';
 import 'package:got_app/theme/theme_const.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage>{
+  int _currentIndex = 0;
+  final pages = const [
+    HomePage(),
+    HighScorePage(),
+  ];
+
+  //BUILD
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile"),
+        title: const Text("A game of thrones"),
       ),
       body: Center(
-        //2 buttons highscores and start game
-
-        child: Container(
-          alignment: Alignment.center,
-          width: 190.0,
-          height: 250.0,
-          color: cAccentLight,
-          child: const Text("PewDiePie"),
+        child: 
+        ElevatedButton(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: const <Widget>[
+              Icon(Icons.play_arrow),
+              SizedBox(
+                width: 3,
+              ),
+              Text("Play game")
+            ],
+          ),
+          onPressed: (){
+          },
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => {
+          _currentIndex = index,
+          Navigator.push(context, MaterialPageRoute(builder: (context) => pages[_currentIndex]))
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.scoreboard),
+            label: "Highscores"
+          ),
+        ],
       ),
     );
   }
+
 }
+
+
+
