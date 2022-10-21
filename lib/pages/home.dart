3e7d2cc9fddcd516ab1a3bb102363449e6a6887a
ValerieBecquart <r0809699@student.thomasmore.be>
2022-10-21 21:50:32 +0200

    widget for pop up page - commented theme_const.dart since we dont use it.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:got_app/pages/highscores.dart';
-import 'package:got_app/theme/theme_const.dart';
+import 'package:got_app/widgets/fullscreendialog.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
   final pages = const [
     HomePage(),
@@ -25,28 +24,43 @@ class _HomePageState extends State<HomePage>{
         title: const Text("A game of thrones"),
       ),
       body: Center(
-        child: 
-        ElevatedButton(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: const <Widget>[
-              Icon(Icons.play_arrow),
-              SizedBox(
-                width: 3,
-              ),
-              Text("Play game")
-            ],
-          ),
-          onPressed: (){
-          },
-        ),
+        child: ElevatedButton(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                Icon(Icons.play_arrow),
+                SizedBox(
+                  width: 3,
+                ),
+                Text("rules with dialog")
+              ],
+            ),
+            onPressed: () {
+              //////////////------------------START: code for pop up screen rules
+              showGeneralDialog(
+                context: context,
+                pageBuilder: (bc, ania, anis) {
+                  return FullScreenDialog(
+                    h1: "Start playing",
+                    p1: 'Hit the start button. Quick… the time is ticking away.  Find as much objects around you as you can. Unlock each item by answering the question. Gather your armour to defeat the end boss.                      ',
+                    p2: 'You get 15 minutes to find and unlock as many armour you find. Not all armour have the same score. Some have a higher value and require more effort to unlock. ',
+                    h2: "Screen layout",
+                    p3: '(1)	See which level you are playing\n(2)	Username \n (3)	Remaining time  \n (4)	Stop the game \n (5)	Current score of this level \n',
+                  );
+                },
+              );
+            }
+            //////////////------------------END: code for pop up screen rules
+
+            ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) => {
           _currentIndex = index,
-          Navigator.push(context, MaterialPageRoute(builder: (context) => pages[_currentIndex]))
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => pages[_currentIndex]))
         },
         items: const [
           BottomNavigationBarItem(
@@ -54,15 +68,9 @@ class _HomePageState extends State<HomePage>{
             label: "Home",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.scoreboard),
-            label: "Highscores"
-          ),
+              icon: Icon(Icons.scoreboard), label: "Highscores"),
         ],
       ),
     );
   }
-
 }
-
-
-
--- lib/theme/theme_const.dart
+++ lib/theme/theme_const.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
-const _cDarkest = Color.fromRGBO(51, 51, 51, 100); //#333333 - Jet
-const _cDarker = Color.fromRGBO(74, 82, 89, 100); //#4A5259
-const _cDark = Color.fromRGBO(196, 202, 208, 100); //#C4CAD0
-const _cLight = Color.fromRGBO(210, 201, 219, 100); //#D2D6DB
+// const _cDarkest = Color.fromRGBO(51, 51, 51, 100); //#333333 - Jet
+// const _cDarker = Color.fromRGBO(74, 82, 89, 100); //#4A5259
+// const _cDark = Color.fromRGBO(196, 202, 208, 100); //#C4CAD0
+// const _cLight = Color.fromRGBO(210, 201, 219, 100); //#D2D6DB
 
-const _cWhite = Color.fromARGB(255, 255, 255, 255); //#FFFFF
+// const _cWhite = Color.fromARGB(255, 255, 255, 255); //#FFFFF
 
-const _cAccentDark = Color.fromRGBO(51, 15, 10, 100); //#330F0A - Dark Siena
-const _cAccentLight = Color.fromRGBO(233, 114, 76, 100); //#CF4B3A - CG Red
+// const _cAccentDark = Color.fromRGBO(51, 15, 10, 100); //#330F0A - Dark Siena
+// const _cAccentLight = Color.fromRGBO(233, 114, 76, 100); //#CF4B3A - CG Red
 
-const _cAccent2Dark =
-    Color.fromRGBO(57, 79, 73, 100); //#394F49 - Dark Slate Grey
-const _cAccent2Light =
-    Color.fromRGBO(156, 196, 178, 100); //9CC4B - Cambrigde Blue
+// const _cAccent2Dark =
+//     Color.fromRGBO(57, 79, 73, 100); //#394F49 - Dark Slate Grey
+// const _cAccent2Light =
+//     Color.fromRGBO(156, 196, 178, 100); //9CC4B - Cambrigde Blue
 
-get cDarkest => _cDarkest;
-get cDarker => _cDarker;
-get cDark => _cDark;
-get cLight => _cLight;
-get cWhite => _cWhite;
-get cAccentDark => _cAccentDark;
-get cAccentLight => _cAccentLight;
+// get cDarkest => _cDarkest;
+// get cDarker => _cDarker;
+// get cDark => _cDark;
+// get cLight => _cLight;
+// get cWhite => _cWhite;
+// get cAccentDark => _cAccentDark;
+// get cAccentLight => _cAccentLight;
 
-get cAccent2Dark => _cAccent2Dark;
-get cAccent2Light => _cAccent2Light;
+// get cAccent2Dark => _cAccent2Dark;
+// get cAccent2Light => _cAccent2Light;
