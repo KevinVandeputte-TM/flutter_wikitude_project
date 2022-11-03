3986f80eb7db227324f9d5cceb7da4f5a48096bf
Kevin Vandeputte <r0792871@student.thomasmore.be>
2022-11-03 14:29:37 +0100

    update user

--- lib/providers/userprovider.dart
+++ lib/providers/userprovider.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:got_app/models/user.dart';
+import 'package:got_app/apis/edgeserver_api';
 
 
 class UserProvider extends ChangeNotifier{
-  int _userid = 0;
+  int _userid = 28;
   int _avatarID = 0;
   int _score = 0;
-  String _username = "";
+  String _username = "userdata";
+  String _email = "userdata@test.com";
   
-
+  //GETTERS
   int get userid => _userid;
   int get score => _score;
   int get avatarID => _avatarID;
   String get username => _username;
+  String get email => _email;
+
+  // METHODS
+  void setUserData(User user){
+    setUserID(user.userID);
+    setUsername(user.name);
+    setAvatarID(user.avatarID);
+    setScore(user.score);
+    setEmail(user.email);
+  }
 
   void setUserID(int id){
     _userid = id;
     notifyListeners();
   }
 
-  void setScore(int score){
-    _score = score;
+  void setScore(int? score){
+    _score = score!;
     notifyListeners();
   }
 
@@ -31,6 +44,34 @@ class UserProvider extends ChangeNotifier{
   void setUsername(String name){
     _username = name;
     notifyListeners();
+  }
+
+  void setEmail(String email){
+    _email = email;
+    notifyListeners();
+  }
+
+  void updateUser(int? scoreOff, int? scoreDef){
+    //Calculate total score of user
+    int total = _score + scoreOff! + scoreDef!;
+    // The service needs an user object. Let's create one with score as total
+    User u = User(userID: _userid, name: _username, email: _email, avatarID: _avatarID, score: total);
+    // Send put request
+    EdgeserverApi.updateUser(u).then((result){
+      // If put is successful then update the provider
+      // ignore: unrelated_type_equality_checks
+      if(result == true){
+        //update the score
+        setScore(total);
+        // TO DO - UPDATE THE GAMEPROVIDERS ITEMLISTS!!!!!!!
+        // else display error message?
+      } else {
+        //TO DO
+
+      }
+
+    });
+
   }
 
 }
